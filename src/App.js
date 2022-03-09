3f7e71fe578b23fedbf2fc4850dba4125dc1cf69
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-03-08 22:59:55 -0800

    styling

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Main from './Components/Main/Main';
 function App() {
   return (
     <div className="app">
-      <header className="App-header"></header>
+      <header className="App-header">Find Your Sign</header>
       <Main />
-      <footer></footer>
+      <footer className="App-footer">
+        zodiac fortune teller is not liable for any actions taken by customer
+      </footer>
     </div>
   );
 }
